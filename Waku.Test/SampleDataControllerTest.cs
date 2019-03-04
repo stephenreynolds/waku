f0e11dfca93e7d95fac73b27116f00fbaf2711fd
Stephen Reynolds <dev@stephenreynolds.me>
2019-03-04 09:39:05 -0500

    First commit

---
+++ Waku.Test/SampleDataControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Waku.Controllers;
+using Xunit;
+using static Waku.Controllers.SampleDataController;
+
+namespace Waku.Test
+{
+    public class SampleDataControllerTest
+    {
+        [Fact]
+        public void WeatherForecastTest()
+        {
+            var controller = new SampleDataController();
+
+            var result = controller.WeatherForecasts();
+
+            Assert.IsAssignableFrom<IEnumerable<WeatherForecast>>(result);
+        }
+    }
+}
